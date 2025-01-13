bb983de4b94a321f7c560b8aaead61f49bc43d41
Claire Wang <clairewang57@gmail.com>
2025-01-13 16:19:48 -0500

    Add cwe-bench-java as submodule

--- build_mvn_project.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# mvn command
-mvn clean package -f pom.xml -B -V -e -Dfindbugs.skip -Dcheckstyle.skip -Dpmd.skip=true -Dspotbugs.skip -Denforcer.skip -Dmaven.javadoc.skip -DskipTests -Dmaven.test.skip.exec -Dlicense.skip=true -Drat.skip=true -Dspotless.check.skip=true
-echo $JAVA_HOME
